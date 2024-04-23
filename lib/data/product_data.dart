f725d9fb1196028bf13897dfa75bc0cf5ca4dd64
DIEGO_RAMON_AVITIA <a.21308051281212@cbtis128.edu.mx>
2024-04-23 18:55:15 +0000

    ac

---
+++ lib/data/product_data.dart
@@ -0,0 +1,17 @@
+import 'package:avitia1212/models/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/1.jpg",
+      title: "CHAPO GUZMAN",
+      description: "Narco Traficante Mexicano Muy Peligroso"),
+  ProductModel(
+      img: "assets/images/2.jpg",
+      title: "Amado Carrillo Fuentes",
+      description:
+          "Este Es El Narco De Narcos El Jefe De Jefes(Nacido en Mexico"),
+  ProductModel(
+      img: "assets/images/3.jpg",
+      title: "Pablo Escobar",
+      description: "Narco Traficante Colombiano Muy Peligroso"),
+];
